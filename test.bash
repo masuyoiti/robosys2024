86c494133e18dd2d749662340cc5416fd2285e9f
yotuwo <yaiti0212@gmail.com>
2024-12-06 15:09:45 +0900

    test.bash修正

--- test.bash
+++ test.bash
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-    echo "${1}行目が違うよ"
+    echo ${1}行目が違うよ
     res=1
 }
 
@@ -12,37 +12,37 @@ res=0
 #gcdとlcmのテスト
 
 #想定内の入力がされた場合
-out=$(echo "12 8" | ./gcd)
-[ "${out}" = "4" ] || ng "$LINENO"
+out=$(echo 12 8 | ./gcd)
+[ "${out}" = 4 ] || ng "$LINENO"
 
-out=$(echo "100 25" | ./lcm)
-[ "${out}" = "100" ] || ng "$LINENO"
+out=$(echo 100 25 | ./lcm)
+[ "${out}" = 100 ] || ng "$LINENO"
 
 #想定外の入力がされた場合
 #ケース1：文字入力
-out=$(echo "a b" | ./gcd)
+out=$(echo a b | ./gcd)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo "c d" | ./lcm)
+out=$(echo c d | ./lcm)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 #ケース2：空白が入力された場合
-out=$(echo "   "| ./gcd)
+out=$(echo    | ./gcd)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo "   " | ./lcm)
+out=$(echo     | ./lcm)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 #ケース3：小数が入力された場合
-out=$(echo "7.0 14.0" | ./gcd)
+out=$(echo 7.0 14.0 | ./gcd)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo "7.0 14.0" | ./lcm)
+out=$(echo 7.0 14.0 | ./lcm)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
